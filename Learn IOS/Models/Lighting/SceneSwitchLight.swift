bf0f72baa5c1106c3b73a413c206eb4c6433f161
kevinle <le8419951779@gmail.com>
2020-03-22 13:03:42 +0800

    讀取亮度與色溫數值

--- Learn IOS/Models/Lighting/SceneSwitchLight.swift
+++ Learn IOS/Models/Lighting/SceneSwitchLight.swift
@@ -16,8 +16,14 @@ class SceneSwitchLight: Light {
         case POWER_ON = "key 80"
     }
     
-    enum GetCommand: String {
+    enum GetCommandRegex: String {
         case POWER_ON = "PowerOn"
+        case POWER_OFF = "PowerOff"
+        case NIGHT_LIGHT_POWER_ON = "NightOn"
+        case NIGHT_LIGHT_POWER_OFF = "NightOf"
+        case LED = "led:.*"
+        case COLOR_TEMPERATURE = "ctemp:.*"
+        case TIMER = "time:.*"
     }
     
     func setLightListener(listener: LightListener){
@@ -28,15 +34,17 @@ class SceneSwitchLight: Light {
         let responses = response.replacingOccurrences(of: ">", with: "").components(separatedBy: "\r\n")
         for command in responses {
             print("analysisResponse: " + command)
-            if isHandle(command: command, target: "PowerOn") {
+            if isHandle(command: command, target: GetCommandRegex.POWER_ON) {
                 listener?.powerOn()
-            } else if isHandle(command: command, target: "NightOf") {
+            } else if isHandle(command: command, target: GetCommandRegex.NIGHT_LIGHT_POWER_OFF) {
                 listener?.nightLightOff()
-            } else if isHandle(command: command, target: "led:.*") {
-                listener?.led(bright: 0)
-            } else if isHandle(command: command, target: "ctemp:.*") {
-                listener?.colorTemperature(colorTemperature: 0)
-            } else if isHandle(command: command, target: "time:.*") {
+            } else if isHandle(command: command, target: GetCommandRegex.LED) {
+                let bright = Int(command.suffix(2), radix: 16)
+                listener?.led(bright: bright ?? 0)
+            } else if isHandle(command: command, target: GetCommandRegex.COLOR_TEMPERATURE) {
+                let colorTemperature = Int(command.suffix(2), radix: 16)
+                listener?.colorTemperature(colorTemperature: colorTemperature ?? 0)
+            } else if isHandle(command: command, target: GetCommandRegex.TIMER) {
                 listener?.timer(timer: 0)
             }
             
@@ -44,8 +52,8 @@ class SceneSwitchLight: Light {
         
     }
     
-    func isHandle(command: String, target: String) -> Bool{
-        return command.range(of: target, options: [.regularExpression]) != nil
+    func isHandle(command: String, target: GetCommandRegex) -> Bool{
+        return command.range(of: target.rawValue, options: [.regularExpression]) != nil
     }
     
 }
