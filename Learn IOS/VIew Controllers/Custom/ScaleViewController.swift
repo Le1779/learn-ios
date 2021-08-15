d906aa81a861fddae4ac461733c1c6d11e12855e
kevinle <le8419951779@gmail.com>
2021-08-16 00:49:34 +0800

    [Add] Scale page

---
+++ Learn IOS/VIew Controllers/Custom/ScaleViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ScaleViewController.swift
+//  Learn IOS
+//
+//  Created by Kevin Le on 2021/8/15.
+//  Copyright © 2021 Kevin Le. All rights reserved.
+//
+
+import UIKit
+
+class ScaleViewController: UIViewController {
+
+    private let backButton = LeButton()
+    private let lineScale = Scale()
+    private let circularScale = Scale()
+    private let leftSlider = CircularSlider()
+    
+    private var constraints = [NSLayoutConstraint]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initViews()
+    }
+
+    @objc func onBackButtonClick(_ sender:UIButton) {
+        UIImpactFeedbackGenerator().impactOccurred()
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+//MARK: Initial Views
+extension ScaleViewController {
+    private func initViews() {
+        initBackButton()
+        initLineScale()
+        initCircularScale()
+        initLeftSlider()
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    private func initBackButton() {
+        view.addSubview(backButton)
+        backButton.cornerType = .round
+        backButton.setImage(withName: "ArrowBack", tintColor: UIColor(hexString: "#585858"))
+        backButton.setBackgroundColor(UIColor(hexString: "#C8C8C840"))
+        backButton.addTarget(self, action: #selector(onBackButtonClick), for: .touchUpInside)
+        
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        constraints.append(backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 48))
+        constraints.append(backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 36))
+        constraints.append(backButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.12))
+        constraints.append(backButton.heightAnchor.constraint(equalTo: backButton.widthAnchor))
+    }
+    
+    private func initLineScale() {
+        lineScale.scaleColor = UIColor(hexString: "#E5E5E5")
+        lineScale.scaleWidth = 3
+        lineScale.units = 0.05
+        view.addSubview(lineScale)
+        
+        lineScale.translatesAutoresizingMaskIntoConstraints = false
+        constraints.append(lineScale.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8))
+        constraints.append(lineScale.heightAnchor.constraint(equalToConstant: 36))
+        constraints.append(lineScale.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(lineScale.topAnchor.constraint(equalTo: backButton.bottomAnchor, constant: 48))
+        
+    }
+    
+    private func initCircularScale() {
+        circularScale.scaleColor = UIColor(hexString: "#E5E5E5")
+        circularScale.scaleWidth = 3
+        circularScale.units = 1/10
+        circularScale.beginAngle = 110
+        circularScale.angle = 140
+        circularScale.scaleType = .circular
+        view.addSubview(circularScale)
+        
+        circularScale.translatesAutoresizingMaskIntoConstraints = false
+        constraints.append(circularScale.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8))
+        constraints.append(circularScale.heightAnchor.constraint(equalTo: circularScale.widthAnchor))
+        constraints.append(circularScale.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(circularScale.topAnchor.constraint(equalTo: lineScale.bottomAnchor, constant: 48))
+    }
+    
+    private func initLeftSlider() {
+        view.addSubview(leftSlider)
+        leftSlider.beginAngle = 250
+        leftSlider.angle = 140
+        leftSlider.clockwise = true
+        
+        leftSlider.translatesAutoresizingMaskIntoConstraints = false
+        constraints.append(leftSlider.widthAnchor.constraint(equalTo: circularScale.widthAnchor, multiplier: 0.87))
+        constraints.append(leftSlider.heightAnchor.constraint(equalTo: circularScale.heightAnchor))
+        constraints.append(leftSlider.centerYAnchor.constraint(equalTo: circularScale.centerYAnchor))
+        constraints.append(leftSlider.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+    }
+}
---
+++ Learn IOS/Views/Scale.swift
@@ -0,0 +1,104 @@
+//
+//  Scale.swift
+//  Learn IOS
+//
+//  Created by Kevin Le on 2021/8/15.
+//  Copyright © 2021 Kevin Le. All rights reserved.
+//
+
+import UIKit
+
+class Scale: UIView {
+    public enum ScaleType {
+        case circular
+        case line
+    }
+    
+    var units: CGFloat = 0.1
+    var hintInterval: Int = 5
+    var scaleType: ScaleType = .line
+    var scaleColor: UIColor = .black
+    var scaleWidth: CGFloat = 1
+    var scaleHeight: CGFloat = 10
+    var scaleDeviation: CGFloat = 0.4
+    
+    var beginAngle: Int = 0
+    var angle: Int = 360
+    
+    var scaleLayer: CAShapeLayer! {
+        didSet {
+            self.layer.addSublayer(self.scaleLayer)
+        }
+    }
+    
+    public init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSublayers(of _: CALayer) {
+        if scaleLayer == nil {
+            scaleLayer = CAShapeLayer()
+            scaleLayer.lineWidth = scaleWidth
+            scaleLayer.strokeColor = scaleColor.cgColor
+            scaleLayer.fillColor = UIColor.clear.cgColor
+            scaleLayer.lineCap = .round
+            if scaleType == .line {
+                drawLineScale()
+            } else {
+                drawCircularScale()
+            }
+        }
+    }
+    
+    /**
+     繪製線的版本
+     */
+    private func drawLineScale() {
+        let scaleWidth = bounds.width*units
+        let path = UIBezierPath()
+        for index in 0..<Int(1/units) + 1 {
+            let isInterval = index % hintInterval == 0
+            let x = CGFloat(index) * scaleWidth
+            path.move(to: CGPoint(x: x, y: bounds.height))
+            path.addLine(to: CGPoint(x: x, y: isInterval ? 0 : bounds.height*scaleDeviation))
+        }
+        
+        scaleLayer.path = path.cgPath
+    }
+    
+    /**
+     繪製圓的版本
+     */
+    private func drawCircularScale() {
+        let height = bounds.height
+        let width = bounds.width
+        let centerPoint = CGPoint(x: bounds.midX, y: bounds.midY)
+        let radius = width < height ? width/2 : height/2
+        
+        let scaleAngle = CGFloat(angle)*units
+        let path = UIBezierPath()
+        for index in 0..<Int(1/units) + 1 {
+            let isInterval = index % hintInterval == 0
+            let angle = beginAngle + index * Int(scaleAngle)
+            let pointA = getPoint(angle: angle, radius: radius - scaleHeight, centerPoint: centerPoint)
+            path.move(to: pointA)
+            
+            let radius = isInterval ? radius : radius - scaleHeight*scaleDeviation
+            let pointB = getPoint(angle: angle, radius: radius, centerPoint: centerPoint)
+            path.addLine(to: pointB)
+        }
+        
+        scaleLayer.path = path.cgPath
+    }
+    
+    private func getPoint(angle: Int, radius: CGFloat, centerPoint: CGPoint) -> CGPoint {
+        let x = radius * cos(Angle(angle).radians)
+        let y = radius * sin(Angle(angle).radians)
+
+        return CGPoint(x: centerPoint.x + x, y: centerPoint.y + y)
+    }
+}
